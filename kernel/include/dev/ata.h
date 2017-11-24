fc48871d8444badbea7561fccddc9094e1e97188
Mohamed Anwar <mohamed_anwar@opmbx.org>
2017-11-25 00:59:42 +0200

    Use ored flags instead of bitfield for status

--- kernel/include/dev/ata.h
+++ kernel/include/dev/ata.h
@@ -23,25 +23,13 @@
 #define ATA_CMD_READ_SECOTRS_EXT    0x24
 #define ATA_DRIVE_LBA48 0x40
 
-#define ATA_STATUS_BSY     0x80    // Busy
-#define ATA_STATUS_DRDY    0x40    // Drive ready
-#define ATA_STATUS_DF      0x20    // Drive write fault
-#define ATA_STATUS_DSC     0x10    // Drive seek complete
-#define ATA_STATUS_DRQ     0x08    // Data request ready
-#define ATA_STATUS_CORR    0x04    // Corrected data
-#define ATA_STATUS_IDX     0x02    // Inlex
-#define ATA_STATUS_ERR     0x01    // Error
-
-union status_reg {
-    struct {
-        unsigned bsy : 1;
-        unsigned rdy : 1;
-        unsigned df  : 1;
-        unsigned srv : 1;
-        unsigned drq : 1;
-        unsigned err : 1;
-    } __packed bits;
-    uint8_t raw;
-} __packed;
+#define ATA_STATUS_BSY     0x80U    // Busy
+#define ATA_STATUS_DRDY    0x40U    // Drive ready
+#define ATA_STATUS_DF      0x20U    // Drive write fault
+#define ATA_STATUS_DSC     0x10U    // Drive seek complete
+#define ATA_STATUS_DRQ     0x08U    // Data request ready
+#define ATA_STATUS_CORR    0x04U    // Corrected data
+#define ATA_STATUS_IDX     0x02U    // Inlex
+#define ATA_STATUS_ERR     0x01U    // Error
 
 #endif
