0671898060e7be0eac61dcfe4cba382a0cbc2e35
Mohamed Anwar <mohamed_anwar@opmbx.org>
2018-05-07 03:37:24 +0200

    [build-tools] fix travis log

--- build-tools/build.sh
+++ build-tools/build.sh
@@ -32,7 +32,7 @@ fi;
 
 
 if [[ ! -d gcc-7.3.0 ]]; then
-	tar xzvf "pkgs/gcc-7.3.0.tar.gz";
+	tar xzf "pkgs/gcc-7.3.0.tar.gz";
     cd "gcc-7.3.0";
     ./contrib/download_prerequisites;
     cd ..;
