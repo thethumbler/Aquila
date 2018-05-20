ff1b905de7bcc06a57f1fdf443514b0b7abe4b6c
Mohamed Anwar <mohamed_anwar@opmbx.org>
2018-05-20 21:50:54 +0200

    [build-tools] fix keep cache

--- build-tools/build.sh
+++ build-tools/build.sh
@@ -56,17 +56,17 @@ if [ "$1" == "distclean" ]
 fi
 
 
-
-set -e #don't continue if a command return non zero
+# don't continue if a command return non zero
+set -e 
 
 #
-#Download Packages
+# Download Packages
 #
 
 echo "Downloading Packages";
 
 if [[ ! -f "pkgs/$BINUTILS.tar.gz" ]]; then
-    wget -P pkgs https://ftp.gnu.org/gnu/binutils/binutils-2.28.tar.gz;
+    wget -P pkgs/ https://ftp.gnu.org/gnu/binutils/binutils-2.28.tar.gz;
 fi;
 
 if [[ ! -f "pkgs/$GCC.tar.gz" ]]; then
@@ -88,121 +88,105 @@ fi;
 echo "The packages are here now"
 
 #
-# Uncompressing
+# Decompressing
 #
 
-echo "Hey packages!, Make yourself home"
-echo "Uncompressing & Configuring.."
-
+echo "Decompressing, Configuring & Building"
 
 if [[ ! -d $AUTOCONF ]]; then
     tar xzf "pkgs/$AUTOCONF.tar.gz";
-    cd $AUTOCONF && ./configure --prefix=$top_dir/sys
+    cd $AUTOCONF;
+    echo "Configuraing autoconf..."
+    ./configure --prefix=$top_dir/sys
+    echo "Building autoconf..."
+    make -j $(nproc) && make install;
+    echo "Done"
     cd ..
 fi
 
 if [[ ! -d $AUTOMAKE ]]; then
     tar xzf "pkgs/$AUTOMAKE.tar.gz";
     cd $AUTOMAKE;
-    if [ $PERL_26 -eq 1 ] #patch if version > 26
-    then
-	patch -N < ../patches/automake.patch
-    fi
+    # patch if version > 26
+    if [ $PERL_26 -eq 1 ]; then
+        patch -N < ../patches/automake.patch;
+    fi;
+    echo "Configuraing automake..."
     ./configure --prefix=$top_dir/sys
+    echo "Building automake..."
+    make -j $(nproc) && make install;
+    echo "Done"
     cd ..
 fi
 
 if [[ ! -d $BINUTILS ]]; then
     tar xzf "pkgs/$BINUTILS.tar.gz";
-    cd $BINUTILS
+    cd $BINUTILS;
+    echo "Configuraing binutils...";
     ./configure --prefix=$top_dir/sys --target=i686-elf --disable-nls --disable-werror --with-sysroot;
-    cd ..
+    echo "Building binutils...";
+    make -j $(nproc) && make install;
+    echo "Done";
+    cd ..;
 fi
 
 if [[ ! -d "build-gcc" ]]; then
     if [[ ! -d $GCC ]]; then
-	tar xzf "pkgs/$GCC.tar.gz";
+        tar xzf "pkgs/$GCC.tar.gz";
     fi;
     cd "$GCC";
     ./contrib/download_prerequisites;
     cd ..;
     mkdir -p "build-gcc" && cd "build-gcc";
+    echo "Configuraing gcc...";
     ../$GCC/configure --prefix=$top_dir/sys --target=i686-elf --disable-nls --enable-languages=c,c++ --without-headers;
+    echo "Building gcc..."
+    make -j $(nproc) all-gcc;
+    make -j $(nproc) all-target-libgcc;
+    make -j $(nproc) install-gcc;
+    make -j $(nproc) install-target-libgcc;
+    echo "Done"
     cd ..;
 fi;
 
+# Always rebuild newlib
+rm -rf $NEWLIB
 if [[ ! -d $NEWLIB ]]; then
     tar xzf "pkgs/$NEWLIB.tar.gz";
     cp aquila $NEWLIB/newlib/libc/sys/ -r;
     cd $NEWLIB;
     patch -p1 < ../patches/newlib.patch;
+    cd newlib/libc/sys/aquila;
+    cd pthread; $top_dir/sys/bin/aclocal -I ../../../..;
+    $top_dir/sys/bin/automake --cygnus Makefile;
+    $top_dir/sys/bin/autoconf;
+    cd ..;
+
+    $top_dir/sys/bin/aclocal -I ../../..;
+    $top_dir/sys/bin/automake --cygnus Makefile;
+    cd ..;
+    $top_dir/sys/bin/autoconf;
+    cd aquila;
+    $top_dir/sys/bin/autoconf;
+    cd $top_dir;
+
+    # Link binaries
+    ln -f sys/bin/i686-elf-ar sys/bin/i686-aquila-ar
+    ln -f sys/bin/i686-elf-as sys/bin/i686-aquila-as
+    ln -f sys/bin/i686-elf-gcc sys/bin/i686-aquila-gcc
+    ln -f sys/bin/i686-elf-gcc sys/bin/i686-aquila-cc
+    ln -f sys/bin/i686-elf-ranlib sys/bin/i686-aquila-ranlib
+    export PATH=$top_dir/sys/bin:$PATH;
+    rm -rf build-newlib && mkdir -p build-newlib && cd build-newlib;
+    echo "Configuring newlib...";
+    ../$NEWLIB/configure --prefix=/usr --target=i686-aquila;
+    echo "Building newlib...";
+    make -j $(nproc) CFLAGS=-march=i586 all;
+    make DESTDIR=$top_dir/libc/sysroot install;
+    cd ..;
+    cp -ar $top_dir/libc/sysroot/usr/i686-aquila/* $top_dir/libc/sysroot/usr/
+    echo "Done"
     cd ..
 fi;
-
-echo "Everything is ready.."
-
-#
-#Building
-#
-
-echo "Let's start Building ...."
-
-echo "Building binutils ...."
-cd $BINUTILS;
-make -j $(nproc) && make install;
-cd ..;
-echo "Done..."
-
-echo "Building gcc..."
-cd "build-gcc"
-make -j $(nproc) all-gcc;
-make -j $(nproc) all-target-libgcc;
-make -j $(nproc) install-gcc;
-make -j $(nproc) install-target-libgcc;
-cd ..;
-echo "Done.."
-
-echo "Building autoconf.."
-cd $AUTOCONF && make -j $(nproc) && make install;
-cd ..;
-echo "Done.."
-
-echo "Building automake.."
-cd $AUTOMAKE;
-make -j $(nproc) && make install;
-cd ..;
-echo "Done.."
-
-echo "Building newlib.."
-
-cd $NEWLIB;
-cd newlib/libc/sys/aquila;
-cd pthread; $top_dir/sys/bin/aclocal -I ../../../..;
-$top_dir/sys/bin/automake --cygnus Makefile;
-$top_dir/sys/bin/autoconf;
-cd ..;
-
-$top_dir/sys/bin/aclocal -I ../../..;
-$top_dir/sys/bin/automake --cygnus Makefile;
-cd ..;
-$top_dir/sys/bin/autoconf;
-cd aquila;
-$top_dir/sys/bin/autoconf;
-cd $top_dir;
-
-# Link binaries
-ln -f sys/bin/i686-elf-ar sys/bin/i686-aquila-ar
-ln -f sys/bin/i686-elf-as sys/bin/i686-aquila-as
-ln -f sys/bin/i686-elf-gcc sys/bin/i686-aquila-gcc
-ln -f sys/bin/i686-elf-gcc sys/bin/i686-aquila-cc
-ln -f sys/bin/i686-elf-ranlib sys/bin/i686-aquila-ranlib
-export PATH=$top_dir/sys/bin:$PATH;
-rm -rf build-newlib && mkdir -p build-newlib && cd build-newlib;
-../$NEWLIB/configure --prefix=/usr --target=i686-aquila;
-make -j $(nproc) CFLAGS=-march=i586 all;
-make DESTDIR=$top_dir/libc/sysroot install;
-cd ..;
-cp -ar $top_dir/libc/sysroot/usr/i686-aquila/* $top_dir/libc/sysroot/usr/
-echo "Done.."
 
 echo "The build was successful, you can now build the image"
