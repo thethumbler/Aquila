a7d382c4b9bb13b2fd93552fb2097d74d3484b8a
Mohamed Anwar <mohamed_anwar@opmbx.org>
2018-12-20 19:35:46 +0200

    [binfmt] support elf64

--- kernel/sys/binfmt/elf.c
+++ kernel/sys/binfmt/elf.c
@@ -26,12 +26,13 @@ int binfmt_elf_check(struct inode *file)
     return -ENOEXEC;
 }
 
-int binfmt_elf_load(proc_t *proc, struct inode *file)
+static int binfmt_elf32_load(proc_t *proc, struct inode *file)
 {
     int err = 0;
 
     elf32_hdr_t hdr;
     vfs_read(file, 0, sizeof(hdr), &hdr);
+
 
     uintptr_t proc_heap = 0;
     size_t offset = hdr.shoff;
@@ -55,8 +56,58 @@ int binfmt_elf_load(proc_t *proc, struct inode *file)
 
             vmr->qnode = enqueue(&proc->vmr, vmr);
 
-            /* FIXME add some out-of-bounds handling code here */
-            //pmman.map(vmr->base, vmr->size, vmr->flags);
+            if (shdr.type == SHT_PROGBITS) {
+                vmr->flags |= VM_FILE;
+                vmr->off   = shdr.off;
+                vmr->inode = file;
+                //vfs_read(vmr->inode, vmr->off, vmr->size, (void *) vmr->base);
+            } else {
+                vmr->flags |= VM_ZERO;
+                //memset((void *) vmr->base, 0, vmr->size);
+            }
+
+            if (shdr.addr + shdr.size > proc_heap)
+                proc_heap = shdr.addr + shdr.size;
+        }
+
+        offset += hdr.shentsize;
+    }
+
+    proc->heap_start = proc_heap;
+    proc->heap = proc_heap;
+    proc->entry = hdr.entry;
+
+    return err;
+}
+
+static int binfmt_elf64_load(proc_t *proc, struct inode *file)
+{
+    int err = 0;
+
+    elf64_hdr_t hdr;
+    vfs_read(file, 0, sizeof(hdr), &hdr);
+
+    uintptr_t proc_heap = 0;
+    size_t offset = hdr.shoff;
+    
+    for (int i = 0; i < hdr.shnum; ++i) {
+        elf64_section_hdr_t shdr;
+        vfs_read(file, offset, sizeof(shdr), &shdr);
+        
+        if (shdr.flags & SHF_ALLOC) {
+            struct vmr *vmr = kmalloc(sizeof(struct vmr));
+            memset(vmr, 0, sizeof(struct vmr));
+
+            vmr->base  = shdr.addr;
+            vmr->size  = shdr.size;
+
+            /* access flags */
+            vmr->flags |= VM_UR;
+            vmr->flags |= shdr.flags & SHF_WRITE ? VM_UW : 0;
+            vmr->flags |= shdr.flags & SHF_EXEC ? VM_UX : 0;
+            //vmr->flags = VM_URWX;   /* FIXME */
+
+            vmr->qnode = enqueue(&proc->vmr, vmr);
 
             if (shdr.type == SHT_PROGBITS) {
                 vmr->flags |= VM_FILE;
@@ -80,4 +131,22 @@ int binfmt_elf_load(proc_t *proc, struct inode *file)
     proc->entry = hdr.entry;
 
     return err;
+}
+
+int binfmt_elf_load(proc_t *proc, struct inode *file)
+{
+    int err = 0;
+
+    elf32_hdr_t hdr;
+    vfs_read(file, 0, sizeof(hdr), &hdr);
+
+    switch (hdr.class) {
+        case 1:
+            return binfmt_elf32_load(proc, file);
+        case 2:
+            return binfmt_elf64_load(proc, file);
+
+    }
+
+    return -EINVAL;
 }
