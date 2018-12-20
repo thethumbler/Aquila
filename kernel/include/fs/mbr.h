2cb91682f5e40f8ac4e8905972cbbfb7cc48d837
Mohamed Anwar <mohamed_anwar@opmbx.org>
2018-12-20 19:40:19 +0200

    fix headers

--- kernel/include/fs/mbr.h
+++ kernel/include/fs/mbr.h
@@ -22,14 +22,14 @@ typedef struct {
     uint32_t  sectors_count;
 } __packed partition_t;
 
-typedef struct {
+struct mbr {
     uint8_t              bootloader[440];   // Actual Bootloader code
     uint32_t             disk_signiture;
     uint16_t             copy_protected;
     partition_t          ptab[4];           // Partition table
     uint16_t             boot_signature;
-} __packed mbr_t;
+} __packed;
 
-void readmbr(struct inode *node);
+//void readmbr(struct inode *node);
 
 #endif
