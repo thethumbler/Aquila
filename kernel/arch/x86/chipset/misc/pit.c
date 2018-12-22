b152e1e5822d59a6baf713fc367b743df85c98bc
Mohamed Anwar <mohamed_anwar@opmbx.org>
2018-12-22 17:38:30 +0200

    [x86] use kargs for 8254 divisor value when available

--- kernel/arch/x86/chipset/misc/pit.c
+++ kernel/arch/x86/chipset/misc/pit.c
@@ -1,8 +1,9 @@
 #include <core/system.h>
+#include <core/kargs.h>
 #include <cpu/io.h>
 #include <chipset/misc.h>
 
-static struct ioaddr __pit_ioaddr;
+static struct ioaddr pit_ioaddr;
 
 #define PIT_CHANNEL0    0x00
 #define PIT_CMD         0x03
@@ -25,18 +26,37 @@ struct pit_cmd_register {
 void x86_pit_setup(struct ioaddr *io)
 {
     printk("8254 PIT: Initializing [%p (%s)]\n", io->addr, ioaddr_type_str(io));
-    __pit_ioaddr = *io;
+    pit_ioaddr = *io;
 }
 
 #define FBASE   1193182ULL  /* PIT Oscillator operates at 1.193182 MHz */
+
+uint32_t atou32(const char *s)
+{
+    uint32_t ret = 0;
+
+    while (*s) {
+        ret = ret * 10 + (*s - '0');
+        ++s;
+    }
+
+    return ret;
+}
+
 uint32_t x86_pit_period_set(uint32_t period_ns)
 {
-    uint32_t freq = 1000000000UL/period_ns;
-    uint32_t div = FBASE/freq;  
+    uint32_t div;
+    const char *arg_div = NULL;
+
+    if (!kargs_get("8254.div", &arg_div)) {
+        div = atou32(arg_div);
+    } else {
+        uint32_t freq = 1000000000UL/period_ns;
+        div = FBASE/freq;  
+    }
 
     if (div == 0) div = 1;
 
-    div = 1000; /* XXX */
     period_ns = 1000000000UL/FBASE/div;
 
     printk("8254 PIT: Setting period to %d ns\n", period_ns);
@@ -48,9 +68,9 @@ uint32_t x86_pit_period_set(uint32_t period_ns)
         .channel = 0,
     };
 
-    io_out8(&__pit_ioaddr, PIT_CMD, cmd.raw);
-    io_out8(&__pit_ioaddr, PIT_CHANNEL0, (div >> 0) & 0xFF);
-    io_out8(&__pit_ioaddr, PIT_CHANNEL0, (div >> 8) & 0xFF);
+    io_out8(&pit_ioaddr, PIT_CMD, cmd.raw);
+    io_out8(&pit_ioaddr, PIT_CHANNEL0, (div >> 0) & 0xFF);
+    io_out8(&pit_ioaddr, PIT_CHANNEL0, (div >> 8) & 0xFF);
 
     return period_ns;
 }
